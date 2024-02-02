13330848b1c77ebabdb483cf4e5890e48606d64b
Sudharsan1705 <137587485+Sudharsan1705@users.noreply.github.com>
2024-02-02 14:06:58 +0530

    Time: 56 ms (20.90%) | Memory: 46.7 MB (83.58%) - LeetSync

---
+++ 2009-longest-word-with-all-prefixes/longest-word-with-all-prefixes.java
@@ -0,0 +1,27 @@
+class Solution {
+    public String longestWord(String[] words) {
+         Arrays.sort(words,(a,b)->{
+          if(a.length() == b.length())
+          return b.compareTo(a);
+          return a.length()-b.length();
+      });
+      Set<String> set = new HashSet<>();
+      set.add("");
+      for(int i=0;i<words.length;i++){
+          set.add(words[i]);
+      }
+      for(int i=words.length-1;i>=0;i--){
+          String str=words[i];
+          int l=str.length()-1;
+          while(l>0){
+              if(!set.contains(str.substring(0,l))){
+                  break;
+              }
+              l--;
+          }
+          if(l == 0)
+          return str;
+      }
+      return "";
+    }
+}
