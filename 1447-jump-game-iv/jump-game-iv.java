cf0097e2a520906b2c0e372c2068fe9f16c66bee
Sudharsan1705 <137587485+Sudharsan1705@users.noreply.github.com>
2024-02-12 15:01:32 +0530

    Time: 992 ms (5.03%) | Memory: 84.8 MB (5.03%) - LeetSync

---
+++ 1447-jump-game-iv/jump-game-iv.java
@@ -0,0 +1,45 @@
+class Solution {
+    public int minJumps(int[] arr) {
+        Queue<Integer> q = new LinkedList<>();
+        q.add(0);
+        Map<Integer,Set<Integer>> map = new HashMap<>();
+        int n=arr.length;
+        for(int i=0;i<n;i++){
+            if(!map.containsKey(arr[i])){
+                map.put(arr[i],new LinkedHashSet<>());
+            }
+            map.get(arr[i]).add(i);
+        }
+        int lev=0;
+        Set<Integer> s = new HashSet<>();
+        Set<Integer> set = new HashSet<>();
+        while(!q.isEmpty()){
+           int size=q.size();
+           while(size-->0){
+               int in=q.poll();
+               if(in == n-1)
+               return lev;
+               s.add(in);
+               if(in-1>=0 && !s.contains(in-1)){
+                   q.add(in-1);
+                   s.add(in-1);
+               }
+               if(in+1<n && !s.contains(in+1)){
+                   q.add(in+1);
+                   s.add(in+1);
+               }
+               if(!set.contains(in)){
+               Set<Integer> num=map.get(arr[in]);
+               for(int i:num){
+                   if(!s.contains(i)){
+                       q.add(i);
+                       set.add(i);
+                   }
+               }
+               }
+           }
+           lev++;
+        }
+        return n-1;
+    }
+}
