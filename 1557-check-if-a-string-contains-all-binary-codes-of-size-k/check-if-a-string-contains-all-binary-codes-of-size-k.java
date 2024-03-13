aacd25bcaaa3520a2fbc75d7b146ed3277bd2881
Sudharsan1705 <137587485+Sudharsan1705@users.noreply.github.com>
2024-03-13 10:21:24 +0530

    Time: 280 ms (13.13%) | Memory: 63.3 MB (80.56%) - LeetSync

--- 1557-check-if-a-string-contains-all-binary-codes-of-size-k/check-if-a-string-contains-all-binary-codes-of-size-k.java
+++ 1557-check-if-a-string-contains-all-binary-codes-of-size-k/check-if-a-string-contains-all-binary-codes-of-size-k.java
@@ -5,14 +5,6 @@ class Solution {
         for(int i=0;i<=n-k;i++){
             set.add(Long.parseLong(s.substring(i,i+k),2));
         }
-        int len=(int)Math.pow(2,k);
-       // System.out.println(len);
-        for(int i=0;i<len;i++){
-            if(!set.contains((long)i)){
-                return false;
-            }
-        }
-        
-        return true;
+        return set.size() == (int)Math.pow(2,k);
     }
 }
