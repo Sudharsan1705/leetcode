a788f7f56789b43de04454cc47adb6b1ed645cc3
Sudharsan1705 <137587485+Sudharsan1705@users.noreply.github.com>
2024-03-22 11:56:35 +0530

    Time: 59 ms (5.05%) | Memory: 51 MB (5.41%) - LeetSync

---
+++ 2576-minimum-penalty-for-a-shop/minimum-penalty-for-a-shop.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int bestClosingTime(String customers) {
+        int n=customers.length();
+        int pre[]=new int[n];
+        char ch[]=customers.toCharArray();
+        int count=0;
+        for(int i=n-1;i>=0;i--){
+           pre[i]=count;
+           if(ch[i] == 'Y'){
+            count++;
+           }
+        }
+        int min=count;
+        count=0;
+        for(int i=0;i<n;i++){
+           if(ch[i] == 'N'){
+            count++;
+           }
+           pre[i]+=count;
+        }
+        System.out.println(Arrays.toString(pre));
+        int res=0;
+        for(int i=0;i<n;i++){
+            if(min>pre[i]){
+                min=pre[i];
+                res=i+1;
+            }
+        }
+        return res;
+    }
+}
